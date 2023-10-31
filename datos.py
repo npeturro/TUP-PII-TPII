a26f38c5ddd558d497bcb3773ee68eff64a9fe7e
npeturro <npeturro@gmail.com>
2023-10-31 17:35:17 -0300

    Correccion de correo

--- datos.py
+++ datos.py
@@ -13,13 +13,13 @@ carreras = [
 cursos = []
 
 estudiantes = [
-    Estudiante("Nicolas", "Rodriguez", "a", "123", "12345", "2023"),
+    Estudiante("Nicolas", "Rodriguez", "prueba@gmail.com", "123", "12345", "2023"),
     Estudiante("Juan", "Perez", "juan@gmail.com", "123", "ABC", "2022"),
     Estudiante("Pedro", "Ramirez", "pedrito@gmail.com", "1111", "4567", "2021")
 ]
 
 profesores = [
-    Profesor("Paula", "Molina", "a", "123", "Kinesiologia", "2006"),
+    Profesor("Paula", "Molina", "prueba@gmail.com", "123", "Kinesiologia", "2006"),
     Profesor("Federico", "Sarasa", "fede@gmail.com", "125", "Nutricionista", "1997"),
     Profesor("Nicolas", "Peturro", "nico@gmail.com", "1111", "Analista en Sistemas", "2021"),
     Profesor("Jordi", "Gimbernat", "jordania@gmail.com", "2222", "Ingeniero en Sistemas", "2100"),
